2c0da6dd36e1ad41ddf29ea9376c1d3244b4244a
Javiertorricos <159574488+Javiertorricos@users.noreply.github.com>
2024-03-16 01:00:06 -0400

    correcion del 10.1

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -93,15 +93,14 @@ string exercise_9(string s1, string s2, string s3, string s4, string s5) {
 
 int exercise_10(int a, int b) {
   // TODO: YOUR CODE HERE
-  if (int a >=0 && int b >=0) 
-  {
-    cout << (a + b) * 2 << endl;
+  if (a < 0 && b < 0) {
+    return 0;
   }
-  else if (int a <=0 && int b <=0) 
-  {
-    cout << (a + b) * 0 << endl;
+  int sum = a + b;
+  int digit_count = (sum == 0)? 1:
+  (int)log10(abs(sum)) + 1;
+  return sum * digit_count;
   }
-}
 
 string exercise_11(int number) {
   // TODO: YOUR CODE HERE
