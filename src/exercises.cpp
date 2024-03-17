cd1ca58e2463716a0c45ecfb6d3d32952b91932e
Javiertorricos <159574488+Javiertorricos@users.noreply.github.com>
2024-03-16 21:45:35 -0400

    correcion 14 ya quiero dormir

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -211,7 +211,15 @@ string exercise_13(int age, int years_of_experience) {
 
 string exercise_14(int number_of_docs) {
   // TODO: YOUR CODE HERE
-  return "";
+  if(number_of_docs <= 0){
+    return "No se encontraron documentos";
+  }
+  else if (number_of_docs == 1) {
+    return "Se encontro un documento";
+  }
+  else if (number_of_docs > 1){
+    return to_string (number_of_docs) + " documentos encontrados";
+  }
 }
 
 void exercise_15(int a, int b, int c) {
